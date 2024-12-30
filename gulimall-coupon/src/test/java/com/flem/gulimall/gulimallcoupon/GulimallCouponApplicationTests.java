55c1b4f07994506d4932da9eef50172c1341c22d
flem <1421853454@qq.com>
2024-12-30 14:25:11 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/flem/gulimall/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flem.gulimall.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-order/src/test/java/com/flem/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flem.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
